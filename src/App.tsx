6546a20e204ecc7e8d9228c4203644d47ba81f29
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-01-28 17:03:30 +0200

    Persist Login on Refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter } from "react-router-dom"
+import AppRoutes from "./routes/AppRoutes"
+import { useEffect } from "react"
+import { useAppDispatch } from "./app/hooks"
+import { loginSuccess } from "./features/auth/authSlice"
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export default function App() {
+  const dispatch = useAppDispatch()
+  useEffect(() => {
+    const storedUser = localStorage.getItem("authUser")
+    if (storedUser) {
+      dispatch(loginSuccess(JSON.parse(storedUser)))
+    }
+  }, [])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
-
-export default App
