2344f47b950ed7db4f3cf534876eb7c548145af5
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-01-28 18:16:09 +0200

    Store

---
+++ src/app/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit"
+import authReducer from "../features/auth/authSlice"
+import userReducer from "../features/user/userSlice"
+import uiReducer from "../features/ui/uiSlice"
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    user: userReducer,
+    ui: uiReducer,
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
