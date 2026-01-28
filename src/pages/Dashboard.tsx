c9527274715acb4439810e0fb8ce580e63b19634
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-01-28 17:12:31 +0200

    Updated dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { useAppSelector } from "../app/hooks"
+import AppLayout from "../components/layout/AppLayout"
 
 export default function Dashboard() {
   const { isAuthenticated, user } = useAppSelector((state) => state.auth)
@@ -13,10 +14,11 @@ export default function Dashboard() {
   }, [isAuthenticated, navigate])
 
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">
-        Welcome, {user?.role.toUpperCase()}
-      </h1>
-    </div>
+      <AppLayout>
+            <div className="p-6">
+                  <h1 className="text-2xl font-bold">Welcome, {user?.role.toUpperCase()}</h1>
+            </div>
+      </AppLayout>
+    
   )
 }
