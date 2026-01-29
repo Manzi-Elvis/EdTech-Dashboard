e988bd93c9c866f64a416e0f013abdbc44bb6776
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-01-29 10:01:28 +0200

    Added sashboard reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "../features/auth/authSlice"
 import userReducer from "../features/user/userSlice"
 import uiReducer from "../features/ui/uiSlice"
+import dashboardReducer from "../features/dashboard/dashboardSlice"
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
     ui: uiReducer,
+    dashboard: dashboardReducer,
   },
 })
 
